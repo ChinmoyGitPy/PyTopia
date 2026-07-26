ab8999450146fa8ebcb36ffd6c090548a459c688
Chinmoy Majumder <chinmoymajumder269@gmail.com>
2026-07-26 12:50:16 +0530

    bug fix

--- assets.py
+++ assets.py
@@ -1,9 +1,9 @@
 import os
 import pygame as py
-
+ 
 asset_dir = r"C:\Users\Chinmoy\OneDrive\Documents\GitHub\PyTopia\Assets"
 asset_filenames = {
-    
+ 
     "forest": "forest.png",
     "mine": "mine.png",
     "metal_ores": "metal ores.png",
@@ -21,20 +21,20 @@ asset_filenames = {
     "plain": "plain.png",
     "treasure_ruins": "treasure ruins.jpg"
 }
-
+ 
 default_tile_width = 314
-
-def load_assets(dir = asset_dir):
+ 
+def load_assets(dir=asset_dir):
     if not py.get_init():
         py.init()
-
+ 
     if py.display.get_surface() is None:
-        py.display.set_mode((1,1))
-
+        py.display.set_mode((1, 1))
+ 
     images = {}
-
-    for key,filename in asset_dir.items():
-        path = os.path.join(dir,filename)
+ 
+    for key, filename in asset_filenames.items():
+        path = os.path.join(dir, filename)
         if not os.path.isfile(path):
             print(f"{key} not loaded, file not found {path}")
             continue
@@ -43,5 +43,5 @@ def load_assets(dir = asset_dir):
         except Exception as e:
             print(f"{key} failed to load {filename}")
             continue
-
-    return images 
+ 
+    return images
